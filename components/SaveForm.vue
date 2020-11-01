0e131ff45759084d5690bc92d1f5007d6a0daaeb
pkimuraa <paulo.kimura94@gmail.com>
2020-11-01 13:45:56 -0200

    Add address form

--- components/SaveForm.vue
+++ components/SaveForm.vue
@@ -8,25 +8,80 @@
           <v-text-field 
           label="Nome do endereco" 
           name="adress-name" 
+          value=""
           />
-          <v-text-field 
-          v-mask="'##.###-###'"
-          label="CEP" 
-          id="zipcode"
-          name="zipcode" 
-          type="numbers"
-          maxlength="10"
-          minlength="10"
+          <v-row
+            class="align-bottom"
+          >
+            <v-col
+              cols="12"
+              lg="9"
+              sm="8"
+            >
+              <v-text-field
+              class=""
+              v-model="cep"
+              v-mask="'##.###-###'" 
+              label="CEP" 
+              id="zipcode"
+              name="zipcode" 
+              type="numbers"
+              maxlength="10"
+              minlength="10"
+              />
+            </v-col>
+            <v-col
+              class="d-flex align-center justify-end"
+              lg="3"
+            >
+              <v-btn 
+                @click="search"
+              >
+              Verificar CEP
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-text-field             
+            v-model="address.logradouro"
+            label="Logradouro"
+            :disabled="true"
           />
+          <v-text-field             
+            v-model="address.bairro"
+            label="Bairro"
+            :disabled="true"
+          />
+          <v-text-field             
+            v-model="number"
+            label="Numero"
+            required="true"
+          />
+          <v-text-field             
+            v-model="complement"
+            label="Complemento"
+          />
+          <v-text-field             
+            v-model="address.localidade"
+            label="Cidade"
+            :disabled="true"
+          />
+          <v-text-field             
+            v-model="address.uf"
+            label="Estado"
+            :disabled="true"
+          />
+            
         </v-form> 
-        <v-card-actions>
-          <v-btn 
-          v-on:click="validate"
-          color="success">
-           Prosseguir </v-btn>
-          <v-btn class="d-none" > Salvar </v-btn>
+        <v-card-actions 
+          class="d-flex justify-end"
+          width="100%"
+        >          
+          <v-btn
+            color="success"
+          > 
+          Salvar 
+          </v-btn>
         </v-card-actions>
-
       </v-card-text>
     </v-card>
 </template>
@@ -34,32 +89,42 @@
 <script>
 import {TheMask} from 'vue-the-mask'
 import axios from 'axios'
-
-
-
+ 
 export default {
-  methods: {
-    async validate({ $axios }) {
-      var inputVal = document.getElementById("zipcode")
-
-      try{
-        console.log(inputVal)
-        const res = await axios.get('https://viacep.com.br/ws/'+ inputVal.value.replace(".", "").replace("-", "") +'/json/');
-        console.log(res.data)
-      } catch(err){
-        console.log(err)
-      }
-    },
+  components: {
+    TheMask
   },
   data () {
     return {
-      components: {TheMask},
-      adress: []
-  }
+      cep: "",
+      address: {},
+      number: "",
+      complement: ""
+    }
 
+  },
+  methods: {
+    search(){
+      if(this.cep && this.cep.length === 10){
+        this.validate();
+      }
+    },
+    async validate() {
+      try{
+        const res = await axios.get(
+          `https://viacep.com.br/ws/${this.cep.replace(".","").replace("-","")}/json`
+          );
+          this.address = (res.data)
+        console.log(res.data) 
+      } catch(err){
 
+        console.log(err)
+      }
+    }, 
+  },
+  watch: {
 
-    //const res = await axios.get('https://viacep.com.br/ws');
-  }
+  },
+
 }
 </script>
